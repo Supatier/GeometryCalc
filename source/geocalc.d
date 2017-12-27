e3bb79e5bc9e23db6245c5da68fb34ec3bd889a6
Supatier <atisuperati@gmail.com>
2017-12-27 20:58:15 +0700

    make const

--- source/geocalc.d
+++ source/geocalc.d
@@ -10,27 +10,27 @@ import std.stdio;
 
 struct VolumeCalc {
         double cone(double radius, double height) {
-                double value = pow(radius, 2) * height * PI / 3;
+                const double value = pow(radius, 2) * height * PI / 3;
                 return value;
         }
 
         double cube(double length, double width, double height) {
-                double value = length * width * height;
+                const double value = length * width * height;
                 return value;
         }
 
         double cylinder(double radius, double height) {
-                double value = pow(radius, 2) * height * PI;
+                const double value = pow(radius, 2) * height * PI;
                 return value;
         }
 
         double sphere(double radius) {
-                double value = pow(radius, 3) * PI * 4 / 3;
+                const double value = pow(radius, 3) * PI * 4 / 3;
                 return value;
         }
 
         double pyramid(double length, double width, double height) {
-                double value = length * width * height / 3;
+                const double value = length * width * height / 3;
                 return value;
         }
 
@@ -38,27 +38,27 @@ struct VolumeCalc {
 
 struct SurfaceAreaCalc {
         double cone(double radius, double height) {
-                double value = radius * PI * (radius * sqrt(pow(height, 2) * pow(radius, 2)));
+                const double value = radius * PI * (radius * sqrt(pow(height, 2) * pow(radius, 2)));
                 return value;
         }
 
         double cube(double length, double width, double height) {
-                double value = 2 * ((length * width) + (length * height) + (width * height));
+                const double value = 2 * ((length * width) + (length * height) + (width * height));
                 return value;
         }
 
         double cylinder(double radius, double height) {
-                double value = (2 * PI * radius * height) + (2 * PI * pow(radius, 2));
+                const double value = (2 * PI * radius * height) + (2 * PI * pow(radius, 2));
                 return value;
         }
 
         double sphere(double radius) {
-                double value = pow(radius, 2) * PI * 4;
+                const double value = pow(radius, 2) * PI * 4;
                 return value;
         }
 
         double pyramid(double length, double width, double height) {
-                double value = (length * width) + (length * sqrt((pow((width / 2),
+                const double value = (length * width) + (length * sqrt((pow((width / 2),
                                 2) + pow(height, 2)))) + (width * sqrt(pow((length / 2),
                                 2) + pow(height, 2)));
                 return value;
